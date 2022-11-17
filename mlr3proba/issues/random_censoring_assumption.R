4ab9f7ccb1f17bc4652af530320a9be50fc4b78a
john <bblodfon@gmail.com>
2022-11-17 17:31:47 +0100

    add test for random censoring assumption from Schmid2012

---
+++ mlr3proba/issues/random_censoring_assumption.R
@@ -0,0 +1,21 @@
+library(mlr3verse)
+library(mlr3proba)
+
+# Task lung ----
+task = tsk('lung')
+pre = po('encode', method = 'treatment') %>>%
+  po('imputelearner', lrn('regr.rpart'))
+task = pre$train(task)[[1]]
+task$missings()
+task # task with clinical data
+
+# CoxPH (mlr3proba) ----
+cox = lrn('surv.coxph')
+
+# Add status indicator as variable
+task$cbind(data.frame(status2 = task$status()))
+task
+cox$train(task)
+
+# Check random censoring assumption (RCA)
+summary(cox$model) # status2 p-value is close to 1 => insignificant => RCA SATISFIED
