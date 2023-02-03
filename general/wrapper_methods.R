f56b34a2721284f3ea09be7a9bb4ec68f371f093
john <bblodfon@gmail.com>
2023-02-03 16:39:36 +0100

    remove merge conflicts

--- general/wrapper_methods.R
+++ general/wrapper_methods.R
@@ -157,11 +157,7 @@ sbs$optimization_path(at_sbs$fselect_instance) # first result of each batch
 ### Calculate #subsets ----
 n = 10000 # number of total features
 n_features = 1 # run until these number of features
-<<<<<<< HEAD
-feature_fraction = 0.7
-=======
 feature_fraction = 0.8
->>>>>>> c4ff104 (investigation: finding the 'best' adaptive mtry.ratio formula)
 feature_number = 5
 
 # first way with `feature_number`:
