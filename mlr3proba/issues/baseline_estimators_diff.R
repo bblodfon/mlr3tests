3330fae4d547c0a018b55be1908a0e866c725849
john <bblodfon@gmail.com>
2023-02-07 11:48:47 +0100

    rename file

--- mlr3proba/issues/baseline_estimators_diff.R
+++ mlr3proba/issues/baseline_estimators_diff.R
@@ -8,7 +8,8 @@ t = tsk("whas")
 l$train(t)
 p = l$predict(t)
 
-# S(t) = exp(-H(t)), H(t) => cumulative hazard
+#' S(t) = exp(-H(t)), H(t) => cumulative hazard
+#' `gbm::basehaz.gbm` => computes the Breslow estimator of the baseline hazard function for a proportional hazard regression model.
 plot(exp(-gbm::basehaz.gbm(t$truth()[, 1], t$truth()[, 2], p$lp,
   t.eval = sort(unique(t$truth()[, 1])) # Breslow (last timepoints get you NA's)
 )), ylim = c(0, 1), type = "l", xlab = "T", ylab = "S(T)")
@@ -18,5 +19,5 @@ t$formula(1) # fit only intercept of the regression model => mean response value
 
 library(survival)
 df = t$data()
-lines(exp(-survival::basehaz(coxph(t$formula(), df)))[, 1], col = 3) # NA? (CoxPH)
+lines(exp(-survival::basehaz(coxph(t$formula(), df)))[, 1], col = 3) # CoxPH
 legend("topright", lty = 1, col = 1:3, legend = c("GBM", "KM", "CPH"))
