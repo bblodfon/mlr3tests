5544f6aafd8581b9b9c18d60bbe294afef1a085c
john <bblodfon@gmail.com>
2023-02-15 11:29:35 +0100

    add comment

--- general/update_mlr3_packages.R
+++ general/update_mlr3_packages.R
@@ -1,5 +1,6 @@
 old_pkgs = unname(old.packages()[,'Package'])
-#old_pkgs
 mlr3_pkgs = old_pkgs[grepl(pattern = 'mlr3', old_pkgs)]
+
+# old mlr3 packages
 mlr3_pkgs
 update.packages(oldPkgs = mlr3_pkgs, ask = 'no')
