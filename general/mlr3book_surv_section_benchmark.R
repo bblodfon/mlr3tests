72571d3fd093e7a9c384bdd78ba59f20538f9b0e
john <bblodfon@gmail.com>
2023-11-03 11:14:30 +0000

    add mlr3proba code in mlr3 book

--- general/mlr3book_surv_section_benchmark.R
+++ general/mlr3book_surv_section_benchmark.R
@@ -12,18 +12,20 @@ tsk_grace$filter(sample(tsk_grace$nrow, 500))
 msr_txt = c("surv.graf", "surv.rcll", "surv.cindex", "surv.dcalib")
 measures = msrs(msr_txt)
 
-graph_learner = as_learner(ppl(
+graph_learner = ppl(
   "distrcompositor",
   learner = lrn("surv.glmnet"),
   estimator = "kaplan",
-  form = "ph"
-))
+  form = "ph",
+  overwrite = TRUE,
+  graph_learner = TRUE
+)
 graph_learner$id = "Coxnet"
 learners = c(lrns(c("surv.coxph", "surv.kaplan")), graph_learner)
 
-bmr = benchmark(benchmark_grid(tsk_grace, learners, rsmp("repeated_cv", repeats = 5, folds = 3)))
+bmr = benchmark(benchmark_grid(tsk_grace, learners, rsmp("repeated_cv", repeats = 3, folds = 3)))
 bmr$aggregate(measures)[, c("learner_id", ..msr_txt)]
 
 learners = c(lrns(c("surv.coxph", "surv.kaplan", "surv.glmnet"))) # distr with Breslow
-bmr = benchmark(benchmark_grid(tsk_grace, learners, rsmp("repeated_cv", repeats = 5, folds = 3)))
+bmr = benchmark(benchmark_grid(tsk_grace, learners, rsmp("repeated_cv", repeats = 3, folds = 3)))
 bmr$aggregate(measures)[, c("learner_id", ..msr_txt)]
