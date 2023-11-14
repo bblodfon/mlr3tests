4e862ea365887a4593825439c8c52358a62418d5
john <bblodfon@gmail.com>
2023-11-14 11:03:39 +0100

    add seeds for reproducibility

--- general/mlr3book_surv_section_benchmark.R
+++ general/mlr3book_surv_section_benchmark.R
@@ -8,8 +8,11 @@ lgr::get_logger("mlr3")$set_threshold("warn")
 
 tsk_grace = tsk("grace")
 tsk_grace$col_roles$stratum = 'status'
+set.seed(42)
 tsk_grace$filter(sample(tsk_grace$nrow, 500))
 msr_txt = c("surv.graf", "surv.rcll", "surv.cindex", "surv.dcalib")
+msr_txt = c("surv.graf", "surv.rcll")
+#msr_txt = c("surv.graf", "surv.cindex")
 measures = msrs(msr_txt)
 
 graph_learner = ppl(
@@ -23,6 +26,7 @@ graph_learner = ppl(
 graph_learner$id = "Coxnet"
 learners = c(lrns(c("surv.coxph", "surv.kaplan")), graph_learner)
 
+set.seed(42)
 bmr = benchmark(benchmark_grid(tsk_grace, learners, rsmp("repeated_cv", repeats = 3, folds = 3)))
 bmr$aggregate(measures)[, c("learner_id", ..msr_txt)]
 
