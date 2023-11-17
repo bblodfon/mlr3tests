fdedda59715b5d48c53f882590334aa9f5358e70
john <bblodfon@gmail.com>
2023-11-17 12:28:17 +0100

    test new dcalib code

--- general/mlr3book_surv_section_benchmark.R
+++ general/mlr3book_surv_section_benchmark.R
@@ -11,7 +11,7 @@ tsk_grace$col_roles$stratum = 'status'
 set.seed(42)
 tsk_grace$filter(sample(tsk_grace$nrow, 500))
 msr_txt = c("surv.graf", "surv.rcll", "surv.cindex", "surv.dcalib")
-msr_txt = c("surv.graf", "surv.rcll")
+msr_txt = c("surv.graf", "surv.dcalib")
 #msr_txt = c("surv.graf", "surv.cindex")
 measures = msrs(msr_txt)
 
