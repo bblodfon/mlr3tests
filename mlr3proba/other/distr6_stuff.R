24a6e5f97c92017f6821acb323c4e314df35d67f
john <bblodfon@gmail.com>
2023-11-29 17:02:47 +0100

    add parameter documentation for a distr6 function

---
+++ mlr3proba/other/distr6_stuff.R
@@ -0,0 +1,6 @@
+#' C_Vec_WeightedDiscreteCdf(x, data, cdf, lower, logp) 
+#' @param x times to extend distribution
+#' @param data times of the currect cdf distr
+#' @param cdf matrix, rows => times (data) and columns => patients
+#' @param lower TRUE => returns cdf, FALSE => return survival (1 - cdf)
+
