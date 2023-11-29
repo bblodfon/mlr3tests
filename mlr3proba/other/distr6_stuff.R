8aafbd9b1a0550035861c1b3cd8f785682fef881
john <bblodfon@gmail.com>
2023-11-29 17:07:20 +0100

    add link to discussion

--- mlr3proba/other/distr6_stuff.R
+++ mlr3proba/other/distr6_stuff.R
@@ -4,3 +4,4 @@
 #' @param cdf matrix, rows => times (data) and columns => patients
 #' @param lower TRUE => returns cdf, FALSE => return survival (1 - cdf)
 
+# see also => https://github.com/mlr-org/mlr3proba/pull/337#discussion_r1390432520
