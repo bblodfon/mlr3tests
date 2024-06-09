d7b0e38663f7661a9da3e08560c5635819cf25da
john <bblodfon@gmail.com>
2024-06-09 21:22:59 +0200

    update library

--- general/serialization_R6.R
+++ general/serialization_R6.R
@@ -1,12 +1,12 @@
-library(pryr)
+library(lobstr)
 
 x = rnorm(1000000)
 y = x
 lx = list(x)
 lxy = list(x, y)
-object_size(lx)
-object_size(lxy)
+obj_size(lx)
+obj_size(lxy)
 
 # serialization problem => R6 problem
-object_size(serialize(lx, NULL))
-object_size(serialize(lxy, NULL)) # it should be ~8MB, not duplicated (16MB)
+obj_size(serialize(lx, NULL))
+obj_size(serialize(lxy, NULL)) # it should be ~8MB, not duplicated (16MB)
