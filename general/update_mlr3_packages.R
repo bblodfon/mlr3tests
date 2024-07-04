849e9502bf4d639645ebfdfe8c502e0edeb5e9f5
john <bblodfon@gmail.com>
2024-07-04 15:20:35 +0200

    update mlr3 packages

--- general/update_mlr3_packages.R
+++ general/update_mlr3_packages.R
@@ -1,6 +1,8 @@
-old_pkgs = unname(old.packages()[,'Package'])
-mlr3_pkgs = old_pkgs[grepl(pattern = 'mlr3', old_pkgs)]
+# update all packages that start with 'mlr3' :)
+old_pkgs = unname(old.packages()[, "Package"])
+mlr3_pkgs = old_pkgs[grepl(pattern = "mlr3", old_pkgs)]
+update.packages(ask = "no", oldPkgs = mlr3_pkgs)
 
-# old mlr3 packages
-mlr3_pkgs
-update.packages(oldPkgs = mlr3_pkgs, ask = 'no')
+# old CRAN-only mlr3 packages
+update.packages(ask = FALSE, oldPkgs = tools::package_dependencies("mlr3verse")[[1]])
+
