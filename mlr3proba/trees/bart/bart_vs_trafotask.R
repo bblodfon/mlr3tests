872b8dcc435dffc5a0d192f37cc9ed78bf3c040b
john <bblodfon@gmail.com>
2024-08-19 11:20:35 +0200

    update script

--- mlr3proba/trees/bart/bart_vs_trafotask.R
+++ mlr3proba/trees/bart/bart_vs_trafotask.R
@@ -1,5 +1,6 @@
 library(BART)
 library(mlr3proba)
+library(mlr3pipelines)
 
 data(lung)
 lung[, 3]
@@ -50,7 +51,7 @@ task_classif$nrow == nrow(pre$tx.train)
 # target same
 all(task_classif$truth() == pre$y.train)
 
-# long data same 
+# long data same
 age1 = task_classif$data(cols = "age")[[1]]
 age2 = pre$tx.train[,"age"]
 all(age1 == age2)
@@ -59,15 +60,19 @@ ph.karno1 = task_classif$data(cols = "ph.karno")[[1]]
 ph.karno2 = pre$tx.train[,"ph.karno"]
 all(ph.karno1 == ph.karno2)
 
+tend1 = task_classif$data(cols = "tend")[[1]]
+tend2 = pre$tx.train[, "t"]
+all(times1 == times2)
+
 # make x_test with fake (time,status) as these are not used
-x_test = cbind(as.data.frame(x.test), time = 1, status = 1)
+x_test = cbind(as.data.frame(x.test), times = 1, status = 1)
 t3 = as_task_surv(x = x_test, event = "status", time = "times")
 pred_task = po_disc$predict(list(t3))[[1L]]
 
 # same nrows
 pred_task$nrow == nrow(pre$tx.test)
 
-# long data same 
+# long data same
 age1 = pred_task$data(cols = "age")[[1]]
 age2 = pre$tx.test[,"age"]
 all(age1 == age2)
