172b0d4892f5d275811996c4b595529487cd0c92
john <bblodfon@gmail.com>
2025-07-03 18:37:45 +0200

    remove file

--- mlr3proba/other/distr6_stuff.R
+++
@@ -1,7 +0,0 @@
-#' C_Vec_WeightedDiscreteCdf(x, data, cdf, lower, logp) 
-#' @param x times to extend distribution
-#' @param data times of the currect cdf distr
-#' @param cdf matrix, rows => times (data) and columns => patients
-#' @param lower TRUE => returns cdf, FALSE => return survival (1 - cdf)
-
-# see also => https://github.com/mlr-org/mlr3proba/pull/337#discussion_r1390432520
